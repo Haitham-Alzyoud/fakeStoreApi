831f31756d29d9fbe9d4de1d1218fe79c2570e91
HaithamAlzyoud <haitham.alzyoud@gmail.com>
2023-10-10 01:38:12 +0300

    tried to submit the output on the screen but didn't work

--- app.js
+++ app.js
@@ -6,27 +6,59 @@ const (title, price ,image){
 }
 }
 
-fetch('https://fakestoreapi.com/products?limit=20')
-    .then((response) => response.json())
-    .then((productData) => productData
+// fetch('https://fakestoreapi.com/products?limit=20')
+//     .then((response) => response.json())
+//     .then((productData) => productData
 
-    .map((Element)=>{
-        let newProducts = new Products(Element.title , Element.price , Element.image);
+//     .map((Element)=>{
+//         let newProducts = new Products(Element.title , Element.price , Element.image);
 
-        let cards = document.createElement('div');
-        cards.innerHTML = `
-        <div>
-            <img src="${Element.image}"/>
-            <h2>${Element.title}</h2>
-            <p>${Element.price}</p>
-        </div>
+//         let cards = document.createElement('div');
+//         cards.innerHTML = `
+//         <div>
+//             <img src="${Element.image}"/>
+//             <h2>${Element.title}</h2>
+//             <p>${Element.price}</p>
+//         </div>
     
-        `;
-        let main =document.getElementById("main");
-        main.appendChild(cards);
-        console.log("cards");
+//         `;
+//         let main =document.getElementById("main");
+//         main.appendChild(cards);
+//         console.log("cards");
 
+//     })
+//     )
+document.addEventListener("DOMContentLoaded" , () =>{
+    let form = document.getElementById("form");
+    let change = document.getElementById("changes");
+    form.addEventListener('submit' , function (e) {
+        e.preventDefault();
+        let title = document.getElementById("title").value;
+        let content = document.getElementById("content").value;
+        let newPost = {title,content};
+        fetch('http://localhost:3000/post',
+        {
+            method:"POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(newPost)
+        })
+        .then(response => response.JSON() )
+        .then(sendData => {
+            let h = `
+            <div>
+            <h2>${sendData.title}</h2>
+            <p>${sendData.content}</p>
+            <button data-id="${sendData.id}" class = "delete">Delete</button>
+            <button data-id="${sendData.id}" class = "update">Edit</button>
+           </div>
+            `
+            change.insertAdjacentHTML("beforeend",h);
+            form.reset();
+        } )
+        
     })
-    )
 
-    // console.log(product);
+
+})
